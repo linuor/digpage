a258e1fb1cae9647b804297dc7b3dbf2fc2fd809
linuor <linuor@gmail.com>
2015-02-21 23:11:51 +0800

    Enable CKEditor

---
+++ backend/web/js/article_form.js
@@ -0,0 +1,8 @@
+CKEDITOR.replace('article-content', {
+    filebrowserBrowseUrl : '/browser/browse/type/all',
+    filebrowserUploadUrl : '/browser/upload/type/all',
+    filebrowserImageBrowseUrl : '/browser/browse/type/image',
+    filebrowserImageUploadUrl : '/browser/upload/type/image',
+    filebrowserWindowWidth  : 800,
+    filebrowserWindowHeight : 500
+});
