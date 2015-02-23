3992939d6e30f65f1adba21ea76df6fc7bb2e7aa
linuor <linuor@gmail.com>
2015-02-23 11:14:07 +0800

    Clean up

--- backend/web/js/article_update.js
+++ backend/web/js/article_update.js
@@ -3,20 +3,5 @@ CKEDITOR.on('instanceCreated', function (event) {
 
     editor.on('configLoaded', function () {
         editor.config.extraPlugins='sourcedialog';
-//        // Remove unnecessary plugins to make the editor simpler.
-//        editor.config.removePlugins = 'colorbutton,find,flash,font,' +
-//                'forms,iframe,image,newpage,removeformat,' +
-//                'smiley,specialchar,stylescombo,templates';
-//
-//        // Rearrange the layout of the toolbar.
-//        editor.config.toolbarGroups = [
-//            {name: 'editing', groups: ['basicstyles', 'links']},
-//            {name: 'undo'},
-//            {name: 'clipboard', groups: ['selection', 'clipboard']},
-//            {name: 'about'}
-//        ];
     });
 });
-//CKEDITOR.inline('editable', {
-//    extraPlugins: 'sourcedialog'
-//});
