5cb3508ae4636b114db974ef8243a96a824ab501
linuor <linuor@gmail.com>
2015-02-25 19:11:13 +0800

    Override API DELETE action, only mark section status to be DELETE, instead of truely drop it.

--- api/controllers/SectionController.php
+++ api/controllers/SectionController.php
@@ -1,8 +1,19 @@
 <?php
 
 namespace api\controllers;
+use common\models\Section;
 
 class SectionController extends \yii\rest\ActiveController
 {
     public $modelClass = 'common\models\Section';
+    
+    public function actions() {
+        $actions = parent::actions();
+        unset($actions['delete']);
+        return $actions;
+    }
+    
+    public function actionDelete($id) {
+        return Section::findOne($id)->markDeleted();
+    }
 }
